e84aebf1b87b5c4b3fc828ce45860fabe1521d69
ashwinsathish24 <ashwindrago97@gmail.com>
2026-04-03 19:11:11 +0530

    Updated installation script

--- install.fish
+++ install.fish
@@ -1,28 +1,42 @@
 #!/usr/bin/env fish
-# 1. Install required packages
-sudo pacman -S wget curl gcc make cmake nano nvim
 
+# Install packages
+sudo pacman -S wget curl gcc make cmake nano nvim fish
+cd ~/
+
+# Clone caelestia
 git clone https://github.com/caelestia-dots/caelestia.git ~/.local/share/caelestia
-~/.local/share/caelestia/install.fish
+cd ~/
 
+# Run installation
+~/.local/share/caelestia/install.fish
+cd ~/
+
+# Install yay
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-cd ~
+cd ~/
 
-git clone https://github.com/ashwinsathish24/LinuxBuild.git ~/Documents/LinuxBuild/
+# Copy LinuxBuild
 sudo mkdir -p /etc/xdg/quickshell/caelestia/
 sudo cp -r ~/Documents/LinuxBuild/* /etc/xdg/quickshell/caelestia/
+cd ~/
 
-cd /home/ashwins/Documents/LinuxBuild/ConfigsMiscs
+# Install custom packages
+cd ~/Documents/LinuxBuild/ConfigsMiscs
 xargs -a Packages.txt sudo pacman -S --needed --noconfirm
+cd ~/
 
-sudo cp /home/ashwins/Documents/LinuxBuild/SystemdScripts/Bin/razer-fix.sh /usr/local/bin/razer-fix.sh
+# Setup razer-fix
+sudo cp ~/Documents/LinuxBuild/SystemdScripts/Bin/razer-fix.sh /usr/local/bin/razer-fix.sh
 sudo chmod +x /usr/local/bin/razer-fix.sh
-sudo cp /home/ashwins/Documents/LinuxBuild/SystemdScripts/Systemd/razer-fix.service /etc/systemd/system/razer-fix.service
-
+sudo cp ~/Documents/LinuxBuild/SystemdScripts/Systemd/razer-fix.service /etc/systemd/system/razer-fix.service
 sudo systemctl daemon-reload
 sudo systemctl start razer-fix.service
+cd ~/
 
+# Check network
 systemctl list-units --type=service | grep -E 'NetworkManager|netctl|dhcpcd|iwd|systemd-networkd'
+cd ~/
